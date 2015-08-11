cc3ecc32dc16d1ae44b328c8b32a6464edfe2cdc
Lan Xiao <lx223@users.noreply.github.com>
2015-08-11 17:01:01 +0100

    Finish chapter 6

---
+++ Chap6/Ex2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap6 {
+    class Book {
+        private string title, author, content, ISBN; 
+
+        public Book(string title, string author, string content, string ISBN) {
+            this.title = title;
+            this.author = author;
+            this.content = content;
+            this.ISBN = ISBN;
+        }
+
+        public string read() {
+            return this.content;
+        }
+
+        public void shelve() {
+            // set state to shelved
+        }
+    }
+}
