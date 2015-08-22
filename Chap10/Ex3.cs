42fc914809f345ecd21e7e6f10631996a7e39260
Lan Xiao <lx223@users.noreply.github.com>
2015-08-22 15:53:09 +0100

    Finish chapter 10

---
+++ Chap10/Ex3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap10 {
+    class Ex3 {
+        static public void run() {
+            string[][] awards = new string[][] { new string[]{ "1st", "2nd"}, new string[]{ "2nd", "3rd" } };
+            for (int dogIndex = 0; dogIndex < awards.Length; dogIndex++) {
+                for (int awardIndex = 0; awardIndex < awards[dogIndex].Length; awardIndex++) {
+                    Console.Write(awards[dogIndex][awardIndex] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Chap10/Ex4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap10 {
+    class Ex4 {
+        static public void run() {
+            string[,] board = new string[9, 9];
+            Random random = new Random();
+            for (int row = 0; row < board.GetLength(0); row++) {
+                for (int col = 0; col < board.GetLength(1); col++) {
+                    board[row, col] = random.Next(0, 2) == 1 ? "black" : "white";
+                }
+            }
+
+            for (int row = 0; row < board.GetLength(0); row++) {
+                for (int col = 0; col < board.GetLength(1); col++) {
+                    Console.Write(board[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
