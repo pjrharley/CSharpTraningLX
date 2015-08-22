4f5afc4e0f82862fc9e6c5c4b612a98fe4ce87fa
Lan Xiao <lx223@users.noreply.github.com>
2015-08-22 15:13:17 +0100

    Finish chapter 9

---
+++ Chap9/Ex3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap9 {
+    class Ex3 {
+        static private int Factorial(int n) {
+            int res = 1;
+            for (int i = 1; i <= n; i++) res *= i;
+            return res;
+        }
+
+        static public void run() {
+            int input = 5;
+            Console.WriteLine(Factorial(input));
+        }
+    }
+}
